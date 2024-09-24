340c0ca60ddd79950612f8b1b5252087c46b61c6
ghiranza <ghiranzathaya@gmail.com>
2024-09-25 00:12:06 +0700

    tugas 4 almost done

--- main/views.py
+++ main/views.py
@@ -3,17 +3,28 @@ from main.forms import VBucksEntryForm
 from main.models import VBucksEntry
 from django.http import HttpResponse
 from django.core import serializers
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+
+import datetime
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
+@login_required(login_url='/login')
 def show_main(request):
-    vbucks_entries = VBucksEntry.objects.all()
+    vbucks_entries = VBucksEntry.objects.filter(user=request.user)
 
     context = {
-        'name' : '500 V-Bucks',
+        'name' : request.user.username,
         'price': 'Rp20.000',
         'description': 'you will get 500 v-bucks with extra 100',
         'bonus':'+100',
-        'vbucks_entries': vbucks_entries
+        'vbucks_entries': vbucks_entries,
+        'last_login': request.COOKIES['last_login'],
     }
 
     return render(request, "main.html", context)
@@ -22,7 +33,9 @@ def create_vbucks_entry(request):
     form = VBucksEntryForm(request.POST or None)
 
     if form.is_valid() and request.method == "POST":
-        form.save()
+        vbucks_entry = form.save(commit=False)
+        vbucks_entry.user = request.user
+        vbucks_entry.save()
         return redirect('main:show_main')
 
     context = {'form': form}
@@ -43,3 +56,37 @@ def show_xml_by_id(request, id):
 def show_json_by_id(request, id):
     data = VBucksEntry.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been successfully created!')
+            return redirect('main:login')
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+def login_user(request):
+   if request.method == 'POST':
+      form = AuthenticationForm(data=request.POST)
+
+      if form.is_valid():
+        user = form.get_user()
+        login(request, user)
+        response = HttpResponseRedirect(reverse("main:show_main"))
+        response.set_cookie('last_login', str(datetime.datetime.now()))
+        return response
+
+   else:
+      form = AuthenticationForm(request)
+   context = {'form': form}
+   return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('main:login'))
+    response.delete_cookie('last_login')
+    return response
